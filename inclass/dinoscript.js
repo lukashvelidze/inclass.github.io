0970ff075c76e865c7e3d303555998dbc4d38ed9
lukashvelidze <lukashvelidze@mail.weber.edu>
2024-10-10 12:22:49 -0600

    changes

--- inclass/dinoscript.js
+++ inclass/dinoscript.js
@@ -2,6 +2,6 @@
     event.preventDefault();
     let partyType = document.getElementById("partyType").value;
     let additional = document.getElementById("Additional").value;
-    let total = partyType + additional * 4;
+    let totalCost = parseInt(partyType) + additional * 4;
     document.getElementById("answer").textContent = `Cost: $ + ${totalCost}`;
  });
